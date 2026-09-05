7808b6cb113bcf6027f0ba84206999cda9cbcb70
k9ksu <k9ksu.dx@gmail.com>
2026-09-05 11:21:52 -0500

    Delete rts_test.py

--- rts_test.py
+++
@@ -1,8 +0,0 @@
-import serial, time
-s = serial.Serial("COM5", 115200)
-s.dtr = False; s.rts = False
-input("Meter on NRST vs GND. Press Enter to assert RTS for 5 s...")
-s.rts = True; s.dtr = True
-time.sleep(5)
-s.rts = False; s.dtr = False
-print("released")
